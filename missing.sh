749a8957c469e3ed87e7e35dc14c05dfeb9361fd
Andreas <akbr18@student.aau.dk>
2022-12-13 12:39:23 +0100

    runs

--- missing.sh
+++ missing.sh
@@ -1,5 +1,5 @@
-ping -c 4 142.93.107.98 >> scalability_4-2_33_cc
-CC=33 k6 run ./get-statistics/scalability_test_4_2.js >> scalability_4-2_33_cc
+ping -c 4 142.93.107.98 >> scalability_1-2_33_cc
+CC=33 k6 run submit-solution/scalability_test_1_2.js >> scalability_1-2_33_cc
 sleep 5m
-ping -c 4 142.93.107.98 >> scalability_4-2_200_cc
-CC=200 k6 run ./get-statistics/scalability_test_4_2.js >> scalability_4-2_200_cc
+ping -c 4 142.93.107.98 >> scalability_1-2_200_cc
+CC=200 k6 run submit-solution/scalability_test_1_2.js >> scalability_1-2_200_cc
